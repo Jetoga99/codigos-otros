2240815c62e294c4c2c11f9bcf24fab2b3921289
jetoga99 <jetoga99@gmail.com>
2022-10-03 17:21:35 -0500

    se resolvió ejercicio 4

---
+++ src/PiedraPapelTijeras.java
@@ -0,0 +1,10 @@
+
+public class PiedraPapelTijeras {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ejercicio4 j1=new Ejercicio4();
+		System.out.println();
+	}
+
+}
